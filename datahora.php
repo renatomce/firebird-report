0f1df8d3f6bdea113924aa357f6a1dd6b65fed1a
Renato <renato.cervati@gmail.com>
2019-02-25 14:30:01 -0300

    Cor dos elementos do rodapé

--- datahora.php
+++ datahora.php
@@ -73,7 +73,7 @@ echo $pagina;
 
 ?>
 <footer><p class="rodape" style="float: left;">Relatorios Gerenciais</p>
-<p class="rodape" style="float: right;">Easy Sistemas</p>
+<p class="rodape" style="float: right;">Easy Sistemas</p></footer>
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
